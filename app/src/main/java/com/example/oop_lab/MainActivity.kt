3729c2a67fcc2a390fec129f5bb62c2d14a6c4e2
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-12-09 20:07:18 +0300

    lab 2

--- app/src/main/java/com/example/oop_lab/MainActivity.kt
+++ app/src/main/java/com/example/oop_lab/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.oop_lab
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import com.example.oop_lab.ui.theme.OOP_LabTheme
 
 
@@ -35,25 +39,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ShowToastButton(){
-    val context = LocalContext.current
-    Button(onClick = {
-        val text = "Hello toast!"
-        val duration = Toast.LENGTH_SHORT
-
-        val toast = Toast.makeText(context, text, duration).show()
-    }) {
-        Text("Tap me")
+fun TextInputForm() {
+    var textFieldState by remember { mutableStateOf("") }
+    var textValue by remember { mutableStateOf("") }
+    TextField(
+        value = textFieldState,
+        onValueChange = { textFieldState = it },
+        label = { Text("Label") }
+    )
+    Text(text = textValue)
+    Button(onClick = { textValue = textFieldState }) {
+        Text(
+            text = "Tap me"
+        )
     }
 }
 
 @Composable
-fun ShowToastButtonWrapper(){
+fun ShowToastButtonWrapper() {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ShowToastButton()
+        TextInputForm()
     }
 }
