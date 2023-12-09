fee9f1b127bc971d00ca0fd52f266b8204f6b47d
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-12-09 20:00:53 +0300

    lab 1

---
+++ app/src/main/java/com/example/oop_lab/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.oop_lab
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.oop_lab.ui.theme.OOP_LabTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OOP_LabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShowToastButtonWrapper()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowToastButton(){
+    val context = LocalContext.current
+    Button(onClick = {
+        val text = "Hello toast!"
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(context, text, duration).show()
+    }) {
+        Text("Tap me")
+    }
+}
+
+@Composable
+fun ShowToastButtonWrapper(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ShowToastButton()
+    }
+}
